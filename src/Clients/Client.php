b353526988f673bc95930482dd5e88a8661753fe
rudi <rudi@bukuku.solutions>
2023-05-09 14:57:07 +0700

    create clients

---
+++ src/Clients/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fatkulnurk\Snaps\Clients;
+
+/**
+ * Class Client
+ *
+ * @category Class
+ * @package  Fatkulnurk\Snaps\Clients
+ * @author   Fatkul Nur Koirudin <rudi@dibumi.com>
+ **/
+class Client
+{
+}
